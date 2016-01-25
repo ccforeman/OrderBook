c2e0076ae935d5239d42f6a039ad8164f15e42d6
ccforeman <cameronforeman@live.com>
2016-01-25 01:48:49 -0600

    added hash map for more efficient lookup & comments

--- OrderBook.scala
+++ OrderBook.scala
@@ -1,18 +1,22 @@
 import scala.collection.mutable.TreeSet
-import scala.util.control.Breaks._
+import scala.collection.mutable.HashMap
 import scala.math.BigDecimal
 import Fields._
 
 object Fields {
-	val Time   = 0
-	val Action = 1
-	val Id     = 2
-	val Side   = 3
-	val Price  = 4
-	val Size   = 5
+	val Time      = 0
+	val Action    = 1
+	val Id        = 2
+	val Side      = 3
+	val Price     = 4
+	val Size      = 5
+
+	val RemAmount = 3
 }
 
 case class OrderBook (target: Long) {
+
+	// Split OrderBook into 2 sides, for buys and sells
 	var buy = new BookSide("B", target)
 	var sell = new BookSide("S", target)
 
@@ -22,37 +26,27 @@ case class OrderBook (target: Long) {
 						case "B" => buy.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
 						case "S" => sell.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
 					}
-			case "R" => buy.book.find(order => order.id == input(Id)) match {
-						case Some(b) => buy.remove(input(Id), input(Side).toLong, input(Time).toLong)
-						case None => sell.remove(input(Id), input(Side).toLong, input(Time).toLong)
+			case "R" => buy.bookIndex.contains(input(Id)) match {
+						case true => buy.remove(input(Id), input(RemAmount).toLong, input(Time).toLong)
+						case false => sell.remove(input(Id), input(RemAmount).toLong, input(Time).toLong)
 					}
 		}
 	}
 }
 
 case class Order(	var time: Long,
-					val id: String,
-					val side: String,
-					val price: BigDecimal,
-					var size: Long
-					) {
+									val id: String,
+									val side: String,
+									val price: BigDecimal,
+									var size: Long
+									) {
 	
+	// Determine what type of Order this is
 	def isBuy = side == "B"
 }
 
 class BookSide(side: String, targetSize: Long) {
 	
-	val sellOrdering = Ordering.by {o: Order => (o.price, o.id) }
-	val buyOrdering = sellOrdering.reverse
-
-	var book: TreeSet[Order] = _
-	
-	if(isBuy)
-		book = TreeSet[Order]()(buyOrdering)
-	else
-		book = TreeSet[Order]()(sellOrdering)
-
-
 	val transactionSide = side match { case "B" => "S"; case "S" => "B" }
 	val target = targetSize
 
@@ -63,21 +57,39 @@ class BookSide(side: String, targetSize: Long) {
 
 	var total: Long = 0
 	var currentTime: Long = 0
+
+	// Determine if OrderBook sorted order will be ascended or descending
+	val sellOrdering = Ordering.by {o: Order => (o.price, o.id) }
+	val buyOrdering = sellOrdering.reverse
+
+	// Determine if OrderBook sorted order will be ascended or descending
+	var book: TreeSet[Order] = _
+	// Effectively constant time lookup to increase scalability; tradeoff for memory footprint
+	var bookIndex = HashMap[String, Order]()
+	
+	if(isBuy)
+		book = TreeSet[Order]()(buyOrdering)
+	else
+		book = TreeSet[Order]()(sellOrdering)
+
 	
 	def add(entry: Order) = {
 		book += entry
+		bookIndex += (entry.id -> entry)
 		total += entry.size
 		currentTime = entry.time
 		update()
 	}
 
 	def remove(id: String, amount: Long, newTime: Long) = {
-		book.find(order => order.id == id) match {
+
+		// Get Order more efficiently the Book Index
+		bookIndex.get(id) match {
 			case Some(o) => o.size -= amount
-							o.time = newTime
-							currentTime = newTime
-							if(o.size <= 0)
-								book.remove(o)
+										o.time = newTime
+										currentTime = newTime
+										if(o.size <= 0)
+											book.remove(o)
 			case None => println("Error: No Removal. Order Not Found.")
 		}
 
@@ -85,6 +97,7 @@ class BookSide(side: String, targetSize: Long) {
 		update()
 	}
 
+	// All flags need to be updated to determine if Pricer will print
 	def update() = {
 		if (total >= target) {
 			oldExpense = newExpense
@@ -102,6 +115,7 @@ class BookSide(side: String, targetSize: Long) {
 			 printOutput
 	}
 
+	// Stream is used for the lazy evaluation of tail
 	def getExpense(shares: Long, bookCopy: Stream[Order]): BigDecimal = {
 
 		if (shares <= 0 || bookCopy.isEmpty)
--- Pricer.scala
+++ Pricer.scala
@@ -1,32 +1,25 @@
 import Fields._
 import scala.io.Source
-import scala.math.BigDecimal
 
 object Pricer {
 	
 	def main(args: Array[String]) = {
-		val start = System.nanoTime()
-
-		if(args.length == 0) {
-			System.err.println("No target-size set")
-			System.exit(1)
+		// Target-size must be included
+		if(args.length ==  0) {
+			System.err.println("Invalid number of arguments.")
+			System.exit(-1)
 		}
 
 		var book = OrderBook(args(0).toLong)
 		val log = Source.stdin
 
-		for(line <- log.getLines()) {
-			var input = line.split("\\s")
-			book.processInput(input)
+		try {
+			for(line <- log.getLines()) {
+				var input = line.split("\\s")
+				book.processInput(input)
+			}
+		} finally {
+			log.close()
 		}
-		log.close()
-		getExecTime(start)
 	}
-
-	def getExecTime(start: Long) = {
-		val time = System.nanoTime() - start
-		println("Execution time: " + time / 1000000000.0)
-		println(11190024 / (time / 1000000000.0))
-	}
-
 }
