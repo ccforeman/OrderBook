415ccd8b601443f9a10e601c908d1df47554e64e
ccforeman <cameronforeman@live.com>
2016-04-02 03:29:52 -0500

    corrected formatting for git, clarified code with comment

--- OrderBook.scala
+++ OrderBook.scala
@@ -3,146 +3,147 @@ import scala.collection.mutable.HashMap
 import scala.math.BigDecimal
 import Fields._
 
+// Used as indexes for input
 object Fields {
-	val Time      = 0
-	val Action    = 1
-	val Id        = 2
-	val Side      = 3
-	val Price     = 4
-	val Size      = 5
+   val Time      = 0
+   val Action    = 1
+   val Id        = 2
+   val Side      = 3
+   val Price     = 4
+   val Size      = 5
 
-	// For removals only
-	val RemAmount = 3
+   // For removals only
+   val RemAmount = 3
 }
 
 case class OrderBook (target: Long) {
 
-	// Split OrderBook into 2 sides, for buys and sells
-	var buy = new BookSide("B", target)
-	var sell = new BookSide("S", target)
+   // Split OrderBook into 2 sides, for buys and sells
+   var buy = new BookSide("B", target)
+   var sell = new BookSide("S", target)
 
-	def processInput(input: Array[String]){
-		input(Action) match {
-		    case "A" => input(Side) match {
-						case "B" => buy.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
-						case "S" => sell.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
-					}
+   def processInput(input: Array[String]){
+      input(Action) match {
+          case "A" => input(Side) match {
+                  case "B" => buy.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
+                  case "S" => sell.add(Order(input(Time).toLong, input(Id), input(Side), BigDecimal(input(Price)), input(Size).toLong))
+               }
 
-				// Lookup in bookIndex because it's more efficient than looking up in book
-				case "R" => buy.bookIndex.get(input(Id)) match {
-						case Some(buyOrder) => buy.remove(buyOrder, input(RemAmount).toLong, input(Time).toLong)
-						case None => sell.bookIndex.get(input(Id)) match {
-							case Some(sellOrder) => sell.remove(sellOrder, input(RemAmount).toLong, input(Time).toLong)
-							case None => System.err.println("Error: Entry not found for removal.")
-						}
-					}
-		}
-	}
+            // Lookup in bookIndex because it's more efficient than looking up in book
+            case "R" => buy.bookIndex.get(input(Id)) match {
+                  case Some(buyOrder) => buy.remove(buyOrder, input(RemAmount).toLong, input(Time).toLong)
+                  case None => sell.bookIndex.get(input(Id)) match {
+                     case Some(sellOrder) => sell.remove(sellOrder, input(RemAmount).toLong, input(Time).toLong)
+                     case None => System.err.println("Error: Entry not found for removal.")
+                  }
+               }
+      }
+   }
 }
 
-case class Order(	var time: Long,
-									val id: String,
-									val side: String,
-									val price: BigDecimal,
-									var size: Long
-									) {
-	
-	// Determine what type of Order this is
-	def isBuy = side == "B"
+case class Order( var time: Long,
+                  val id: String,
+                  val side: String,
+                  val price: BigDecimal,
+                  var size: Long
+               ) {
+   
+   // Determine what type of Order this is
+   def isBuy = side == "B"
 }
 
 class BookSide(side: String, targetSize: Long) {
-	
-	val transactionSide = side match { case "B" => "S"; case "S" => "B" }
-	val target = targetSize
+   
+   val transactionSide = side match { case "B" => "S"; case "S" => "B" }
+   val target = targetSize
 
-	var oldExpense = BigDecimal("0.0")
-	var newExpense = BigDecimal("0.0")
-	var newOutcome: String = "NA"
-	var oldOutcome: String = "NA"
+   var oldExpense = BigDecimal("0.0")
+   var newExpense = BigDecimal("0.0")
+   var newOutcome: String = "NA"
+   var oldOutcome: String = "NA"
 
-	var totalShares: Long = 0
-	var currentTime: Long = 0
+   var totalShares: Long = 0
+   var currentTime: Long = 0
 
-	val sellOrdering = Ordering.by {o: Order => (o.price, o.id) }
-	val buyOrdering = sellOrdering.reverse
+   val sellOrdering = Ordering.by {o: Order => (o.price, o.id) }
+   val buyOrdering = sellOrdering.reverse
 
-	// Determine if OrderBook sorted order will be ascended or descending
-	var book: TreeSet[Order] = _
-	// Effectively constant time lookup to increase scalability; tradeoff for memory footprint
-	var bookIndex = HashMap[String, Order]()
-	
-	// Determine if BookSide sorted order will be ascended or descending
-	if(isBuy)
-		book = TreeSet[Order]()(buyOrdering)
-	else
-		book = TreeSet[Order]()(sellOrdering)
+   // Determine if OrderBook sorted order will be ascended or descending
+   var book: TreeSet[Order] = _
+   // Effectively constant time lookup to increase scalability; tradeoff for memory footprint
+   var bookIndex = HashMap[String, Order]()
+   
+   // Determine if BookSide sorted order will be ascended or descending
+   if(isBuy)
+      book = TreeSet[Order]()(buyOrdering)
+   else
+      book = TreeSet[Order]()(sellOrdering)
 
-	
-	def add(entry: Order) = {
-		book += entry
-		bookIndex += (entry.id -> entry)
-		totalShares += entry.size
-		currentTime = entry.time
-		update()
-	}
+   
+   def add(entry: Order) = {
+      book += entry
+      bookIndex += (entry.id -> entry)
+      totalShares += entry.size
+      currentTime = entry.time
+      update()
+   }
 
-	def remove(order: Order, amount: Long, newTime: Long) = {
-		order.size -= amount
-		order.time = newTime
-		currentTime = newTime
-		if(order.size <= 0) {
-			book.remove(order)
-			bookIndex.remove(order.id)
-		}
+   def remove(order: Order, amount: Long, newTime: Long) = {
+      order.size -= amount
+      order.time = newTime
+      currentTime = newTime
+      if(order.size <= 0) {
+         book.remove(order)
+         bookIndex.remove(order.id)
+      }
 
-		totalShares -= amount
-		update()
-	}
+      totalShares -= amount
+      update()
+   }
 
-	// All flags need to be updated to determine if Pricer will print
-	def update() = {
-		if (totalShares >= target) {
-			oldExpense = newExpense
-			oldOutcome = newOutcome
-			newOutcome = transactionSide
-			newExpense = getExpense(target, book.toStream)
-		} else {
-			oldExpense = newExpense
-			oldOutcome = newOutcome
-			newOutcome = "NA"
-			newExpense = BigDecimal("0.0")
-		}
+   // All flags need to be updated to determine if Pricer will print
+   def update() = {
+      if (totalShares >= target) {
+         oldExpense = newExpense
+         oldOutcome = newOutcome
+         newOutcome = transactionSide
+         newExpense = getExpense(target, book.toStream)
+      } else {
+         oldExpense = newExpense
+         oldOutcome = newOutcome
+         newOutcome = "NA"
+         newExpense = BigDecimal("0.0")
+      }
 
-		if((newExpense != oldExpense) || (newOutcome != oldOutcome))
-			 printOutput
-	}
+      if((newExpense != oldExpense) || (newOutcome != oldOutcome))
+          printOutput
+   }
 
-	// Stream is used for the lazy evaluation of tail
-	def getExpense(shares: Long, bookCopy: Stream[Order]): BigDecimal = {
+   // Stream is used for the lazy evaluation of tail
+   def getExpense(shares: Long, bookCopy: Stream[Order]): BigDecimal = {
 
-		if (shares <= 0 || bookCopy.isEmpty)
-			0
-		else if((shares - bookCopy.head.size) > 0)
-			(bookCopy.head.price * bookCopy.head.size) + getExpense(shares - bookCopy.head.size, bookCopy.tail)
-		else
-			(bookCopy.head.price * shares) + getExpense(shares - bookCopy.head.size, bookCopy.tail)
+      if (shares <= 0 || bookCopy.isEmpty)
+         0
+      else if((shares - bookCopy.head.size) > 0)
+         (bookCopy.head.price * bookCopy.head.size) + getExpense(shares - bookCopy.head.size, bookCopy.tail)
+      else
+         (bookCopy.head.price * shares) + getExpense(shares - bookCopy.head.size, bookCopy.tail)
 
-	}
+   }
 
-	def printOutput = {
-		val str = StringBuilder.newBuilder
+   def printOutput = {
+      val str = StringBuilder.newBuilder
 
-		if(newOutcome == transactionSide) {
-			str.append(currentTime).append(" ").append(transactionSide).append(" ").append(newExpense)
-			println(str)
-		} else {
-			str.append(currentTime).append(" ").append(transactionSide).append(" ").append("NA")
-			println(str)
-		}
-	}
+      if(newOutcome == transactionSide) {
+         str.append(currentTime).append(" ").append(transactionSide).append(" ").append(newExpense)
+         println(str)
+      } else {
+         str.append(currentTime).append(" ").append(transactionSide).append(" ").append("NA")
+         println(str)
+      }
+   }
 
-	// Determine if the BookSide object is buy-side or sell-side
-	def isBuy = side == "B"
+   // Determine if the BookSide object is buy-side or sell-side
+   def isBuy = side == "B"
 
 }
--- Pricer.scala
+++ Pricer.scala
@@ -3,44 +3,44 @@ import scala.io.Source
 
 /*
 
-	Pricer reads from stdin line by line that is either an add:
+   Pricer reads from stdin line by line that is either an add:
 
-	   timestamp A order-id side price size
+      timestamp A order-id side price size
 
-	Or a remove:
+   Or a remove:
 
-	   timestamp R order-id size
+      timestamp R order-id size
 
-	Output should be:
+   Output should be:
 
-	   timestamp action total
+      timestamp action total
 
 */
 
 object Pricer {
-	
-	def main(args: Array[String]) = {
+   
+   def main(args: Array[String]) = {
 
-		// Target-size must be included
-		if(args.length ==  0) {
-			System.err.println("Invalid number of arguments.")
-			System.exit(-1)
-		}
+      // Target-size must be included
+      if(args.length ==  0) {
+         System.err.println("Invalid number of arguments.")
+         System.exit(-1)
+      }
 
-		// Book instantiation requires target-size
-		var book = OrderBook(args(0).toLong)
-		val log = Source.stdin
+      // Book instantiation requires target-size
+      var book = OrderBook(args(0).toLong)
+      val log = Source.stdin
 
-		try {
-			for(line <- log.getLines()) {
-				var input = line.split("\\s")
-				book.processInput(input)
-			}
-		} catch {
-			case e: Exception => System.err.println("Warning: Encountered error in input.")
-		}finally {
-			log.close()
-		}
+      try {
+         for(line <- log.getLines()) {
+            var input = line.split("\\s")
+            book.processInput(input)
+         }
+      } catch {
+         case e: Exception => System.err.println("Warning: Encountered error in input.")
+      }finally {
+         log.close()
+      }
 
-	}
+   }
 }
